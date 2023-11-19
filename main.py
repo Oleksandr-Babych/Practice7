fc51c9a65102f689b974ace8199eecca6da605e2
andriBilozor <andrii.bilozor@e-u.edu.ua>
2023-11-19 05:16:27 +0200

    changes

--- main.py
+++ main.py
@@ -2,50 +2,45 @@ import PySimpleGUI as sg
 from module import generate_random_numbers, calculate_average
 from database import config
 
-def main():
-    # Оновлений макет з додаванням кнопки "Export to File"
-    layout = [
+def create_layout():
+    return [
         [sg.Text("Натисніть на кнопку, щоб згенерувати випадкові числа та обчислити середнє арифметичне.")],
         [sg.Button("Генерувати числа"), sg.Button("Export to File"), sg.Exit()],
         [sg.Output(size=(40, 10))]
     ]
 
-    # Створення вікна з новим макетом
+def main():
+    layout = create_layout()
     window = sg.Window("Графічний Інтерфейс", layout, resizable=True)
+    
+    random_numbers = []  
 
     while True:
-        # Очікування подій вікна
         event, values = window.read()
 
-        # Обробка подій
         if event == sg.WIN_CLOSED or event == "Exit":
             break
         elif event == "Генерувати числа":
-            # Генерація чисел та обчислення середнього
             random_numbers = generate_random_numbers(10)
             average = calculate_average(random_numbers)
-
-            # Виведення результатів у вікно
             print("Список випадкових чисел:", random_numbers)
             print("Середнє арифметичне:", average)
             print("\n" + "="*40 + "\n")
         elif event == "Export to File":
-            # Виклик функції для збереження результатів у файл
-            save_to_file(random_numbers, average)
+            try:
+                save_to_file(random_numbers, average)
+            except Exception as e:
+                print(f"Error saving to file: {e}")
 
-    # Закриття вікна при виході з циклу
     window.close()
 
 def save_to_file(numbers, avg):
-    # Вивід діалогового вікна для отримання імені файлу
-    filename = sg.popup_get_file("Зберегти у файл", save_as=True, default_extension=".txt")
+    filename = sg.popup_get_file("Зберегти у файл", save_as=True, default_extension=".txt", file_types=(("Text Files", "*.txt"),))
 
     if filename:
-        # Додаємо розширення .txt, якщо воно відсутнє
         if not filename.lower().endswith(".txt"):
             filename += ".txt"
 
-        # Запис результатів у вказаний файл
         with open(filename, "w") as file:
             file.write("Список випадкових чисел: {}\n".format(numbers))
             file.write("Середнє арифметичне: {}\n".format(avg))
--- module.py
+++ module.py
@@ -7,3 +7,14 @@ def generate_random_numbers(n):
 def calculate_average(numbers):
     """Обчислює середнє арифметичне чисел у списку."""
     return sum(numbers) / len(numbers)
+
+def calculate_standard_deviation(numbers):
+    # Calculate the standard deviation
+    if len(numbers) < 2:
+        return 0  # Standard deviation is not defined for less than 2 numbers
+
+    mean = sum(numbers) / len(numbers)
+    variance = sum((x - mean) ** 2 for x in numbers) / len(numbers)
+    std_deviation = math.sqrt(variance)
+
+    return std_deviation
